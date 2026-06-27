b1629cd6258245729236f04cfa1d660c282e1391
Prashant Aghara <prashant.aghara2111@gmail.com>
2026-06-27 13:41:04 +0530

    Spring Boot Application Initial Setup

---
+++ backend/src/main/java/ai/finsight/backend/FinSightApplication.java
@@ -0,0 +1,13 @@
+package ai.finsight.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinSightApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ai/finsight/backend/FinSightApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.finsight.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
